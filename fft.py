7e08a853d10bd3a9939e55dd346cdd03de9804e0
Richard Beattie <richard.beattie91@gmail.com>
2025-10-31 13:26:21 -0400

    Add deps and test code

---
+++ fft.py
@@ -0,0 +1,84 @@
+import argparse
+from src.iwr1443 import Radar
+import numpy as np
+from PyQt6 import QtWidgets
+from src.distance_plot import DistancePlot
+import sys
+from scipy.fft import fft, fftfreq
+
+
+def sliding_window_average(frame, window_size):
+    """
+    Compute the moving average of the data across frames
+
+    Args:
+        frame (np.ndarray): The input data array (n_chirps_per_frame, n_fft_bins)
+        window_size (int): The size of the moving average window.
+    """
+    kernel = np.ones(window_size) / window_size
+    filtered = np.apply_along_axis(
+        lambda m: np.convolve(m, kernel, mode="same"), axis=0, arr=frame
+    )
+    pad = window_size // 2
+    return filtered[pad:-pad]
+
+
+def background_subtraction(frame):
+    after_subtraction = np.zeros_like(frame)
+    for i in range(1, frame.shape[0]):
+        after_subtraction[i - 1] = frame[i] - frame[i - 1]
+
+    return after_subtraction
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cfg", type=str, required=True)
+    args = parser.parse_args()
+
+    # Initalize the radar
+    radar = Radar(args.cfg)
+
+    params = radar.params
+
+    c = 3e8  # speed of light - m/s
+    SAMPLES_PER_CHIRP = params["n_samples"]  # adc number of samples per chirp
+    SAMPLE_RATE = params["sample_rate"]  # digout sample rate in Hz
+    FREQ_SLOPE = params["chirp_slope"]  # frequency slope in Hz (/s)
+
+    # Initalize the GUI
+    app = QtWidgets.QApplication(sys.argv)
+    dist_plot = DistancePlot(params["range_res"])
+    dist_plot.resize(600, 600)
+    dist_plot.show()
+
+    def update_frame(msg):
+        frame = msg.get("data", None)
+        if frame is None:
+            return
+        
+        
+        frame = background_subtraction(frame)
+
+        # Get the fft of the data
+        signal = np.mean(frame, axis=0)
+
+        fft_result = fft(signal, axis=0)
+        fft_freqs = fftfreq(SAMPLES_PER_CHIRP, 1 / SAMPLE_RATE)
+        fft_meters = fft_freqs * c / (2 * FREQ_SLOPE)
+
+        # Plot the data
+        dist_plot.update(
+            fft_meters[: SAMPLES_PER_CHIRP // 2],
+            np.abs(fft_result[: SAMPLES_PER_CHIRP // 2, :]),
+        )
+
+        app.processEvents()
+
+    # Initialize the radar
+
+    radar.run_polling(cb=update_frame)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/distance_plot.py
@@ -0,0 +1,71 @@
+import pyqtgraph as pg
+from PyQt6 import QtWidgets, QtCore
+import numpy as np
+import sys
+
+
+class DistancePlot(QtWidgets.QMainWindow):
+    """
+    A line plot of multiple distance vs. intensity curves.
+    """
+
+    def __init__(self, range_res: float, num_lines: int = 4):
+        super().__init__()
+
+        self.range_res = range_res
+        self.setWindowTitle("Distance Plot")
+        self.plot_widget = pg.PlotWidget()
+        self.setCentralWidget(self.plot_widget)
+
+        colors = [(255, 0, 0), (0, 255, 0), (0, 150, 255), (255, 255, 0)]
+        self.lines = [
+            pg.PlotDataItem(pen=pg.mkPen(color=colors[i], width=2), symbol=None)
+            for i in range(num_lines)
+        ]
+
+        for line in self.lines:
+            self.plot_widget.addItem(line)
+
+        self.plot_widget.setLabel("bottom", "Distance (m)")
+        self.plot_widget.setLabel("left", "Intensity")
+
+    def update(self, distances: np.ndarray, data: np.ndarray):
+        """
+        Args:
+            data_list (np.ndarray): 2D array of (n_distances, n_receivers).
+        """
+        if data is None:
+            return
+
+        if data.ndim != 2:
+            self.lines[0].setData(distances, data)
+        else:
+            for i in range(data.shape[1]):
+                self.lines[i].setData(distances, data[:, i])
+
+        self.plot_widget.setXRange(0, 6)
+        self.plot_widget.setYRange(0, 10000)
+
+
+if __name__ == "__main__":
+    # Initalize the GUI
+    app = QtWidgets.QApplication(sys.argv)
+    iq_plot = DistancePlot(range_res=0.1)  # Example range resolution
+    iq_plot.resize(600, 600)
+    iq_plot.show()
+
+    distances = np.linspace(0, 5, 90)
+
+    # Add 10 entries between 5 and 10
+    distances = np.concatenate((distances, np.linspace(5, 10, 10)))
+
+    # Example data
+    base = np.linspace(0, 10, 100)
+    data_list = [base, base * 0.8, np.sin(base), np.exp(-0.1 * base) * 10]
+
+    data = np.column_stack(data_list)
+
+    iq_plot.update(distances, data)
+
+    # sys.exit(app.exec_())
+    sys.exit(app.exec())
