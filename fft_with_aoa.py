d3a3fe0c83ba3f6966e395da75003e4cf1ee338c
Richard Beattie <richard.beattie91@gmail.com>
2025-11-15 10:23:00 -0500

    Test with zoomfft

---
+++ fft_with_aoa.py
@@ -0,0 +1,65 @@
+import argparse
+from src.iwr1443.radar import Radar
+import numpy as np
+from PyQt6 import QtWidgets
+from src.distance_plot import DistancePlot
+import sys
+from scipy.fft import fft, fftfreq, zoom_fft
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cfg", type=str, required=True)
+    args = parser.parse_args()
+
+    # Initalize the radar
+    print("initinalizing radar")
+    radar = Radar(args.cfg, host_ip="192.168.33.42")
+    print("inited radar")
+
+    params = radar.params
+
+    c = 3e8  # speed of light - m/s
+    SAMPLES_PER_CHIRP = params["n_samples"]  # adc number of samples per chirp
+    SAMPLE_RATE = params["sample_rate"]  # digout sample rate in Hz
+    FREQ_SLOPE = params["chirp_slope"]  # frequency slope in Hz (/s)
+
+    # Initalize the GUI
+    app = QtWidgets.QApplication(sys.argv)
+    dist_plot = DistancePlot(params["range_res"])
+    dist_plot.resize(600, 600)
+    dist_plot.show()
+
+    def update_frame(msg):
+        frame = msg.get("data", None)
+        if frame is None:
+            return
+
+        # We expect reflectors to be at
+        expected_reflector = 10 # expect a reflector at 10cm
+        bounds = 2 # +- 2cm
+
+        # Perform a ZoomFFT around the expected reflector
+        f1 = (expected_reflector - bounds) / c * (2 * FREQ_SLOPE)
+        f2 = (expected_reflector + bounds) / c * (2 * FREQ_SLOPE)
+        
+        m = 4096                     # high-res output (zoom)
+        Xz = zoom_fft(frame, [f1, f2], m=m, fs=SAMPLE_RATE, axis=1) # Across the samples per chirp axis
+
+        fft_freqs = np.linspace(f1, f2, m, endpoint=False)
+        fft_meters = fft_freqs * c / (2 * FREQ_SLOPE)
+
+        # Plot the data
+        dist_plot.update(
+            fft_meters,
+            np.abs(Xz),
+        )
+        
+        app.processEvents()
+
+    # Initialize the radar
+
+    radar.run_polling(cb=update_frame)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/zoomfft.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import zoom_fft
+
+fs = 1000
+T = 2
+t = np.linspace(0, T, int(fs*T), endpoint=False)
+
+x = (
+    np.sin(2*np.pi*1*t) +
+    np.sin(2*np.pi*10*t) +
+    np.sin(2*np.pi*100*t)
+)
+
+# zoom settings
+f1, f2 = 5, 15               # 10 Hz ± 5 Hz
+m = 4096                     # high-res output (zoom)
+
+Xz = zoom_fft(x, [f1, f2], m=m, fs=fs)
+freqs = np.linspace(f1, f2, m, endpoint=False)
+
+# Also for comparison calculate the normal FFT but truncated to the same region
+X = np.fft.fft(x)
+freqs_full = np.fft.fftfreq(len(x), 1/fs)
+
+plt.figure()
+plt.subplot(2,1,1)
+plt.title("Normal FFT")
+plt.plot(freqs_full, np.abs(X))
+plt.xlim(f1, f2)
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Magnitude")
+plt.subplot(2,1,2)
+plt.title("Zoom FFT")
+plt.plot(freqs, np.abs(Xz))
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Magnitude")
+plt.show()
